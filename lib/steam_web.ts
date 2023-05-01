47750a373c8a177118b5776b9205a46580a5b67e
Bilel MEDIMEGH <bilel.medimegh@gmail.com>
2023-05-01 13:32:39 +0200

    fix build

--- lib/steam_web.ts
+++ lib/steam_web.ts
@@ -1,10 +1,19 @@
 import {Steam} from "steamwebapi-ts";
+import * as process from "process";
+import { PHASE_PRODUCTION_BUILD } from 'next/constants';
+
 
 const globalForSteam = global as unknown as { steam_web: Steam };
 
+let api_key = process.env.STEAM_WEB_API_KEY
+if(process.env.NEXT_PHASE == PHASE_PRODUCTION_BUILD){
+  api_key = "invalid_token"
+}
 export const steam_web = (
   globalForSteam.steam_web ||
-  new Steam(process.env.STEAM_WEB_API_KEY)
+  new Steam(api_key)
 );
+
+
 
 if (process.env.NODE_ENV !== 'production') globalForSteam.steam_web = steam_web;
