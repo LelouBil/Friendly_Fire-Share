73257a2d41890dbedf782087d6994cd405c43f97
Bilel MEDIMEGH <bilel.medimegh@gmail.com>
2023-05-02 14:05:53 +0200

    fix

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,12 @@
 module.exports = {
+    debug: process.env.NODE_ENV === 'development',
     i18n: {
         defaultLocale: 'fr',
         locales: ['fr'],
     },
+    localePath:
+        typeof window === 'undefined'
+            ? require('path').resolve('./public/locales')
+            : '/locales',
+    reloadOnPrerender: process.env.NODE_ENV === 'development',
 }
